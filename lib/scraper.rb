1102a164fceb4480e2ec1741cd3a9516b59bfa79
Dani Lyn <dlyn1110@gmail.com>
2018-06-11 13:00:49 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    #is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array
+    # of hashes in which each hash represents one student
+    # need to return an array to iterate over to grab the information I want.
+    doc = Nokogiri::HTML(open(index_url))
+    students_array = []
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
