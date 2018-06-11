214aa0d26c0f398d83720c19a37fa013bc77953b
Dani Lyn <dlyn1110@gmail.com>
2018-06-11 13:12:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,8 +9,15 @@ class Scraper
     # need to return an array to iterate over to grab the information I want.
     doc = Nokogiri::HTML(open(index_url))
     students_array = []
-    
-
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
